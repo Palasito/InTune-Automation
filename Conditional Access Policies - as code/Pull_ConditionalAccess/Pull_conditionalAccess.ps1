8761b5ed6bca6c7bff14608d266d19428d927b20
Chris Palas <chris.palavouzis@gmail.com>
2021-12-06 01:49:19 +0200

    ConditionalAccess new export display

--- Conditional Access Policies - as code/Pull_ConditionalAccess/Pull_conditionalAccess.ps1
+++ Conditional Access Policies - as code/Pull_ConditionalAccess/Pull_conditionalAccess.ps1
@@ -5,10 +5,20 @@
 # Connect to Azure AD
 Connect-AzureAD
 
+if (-not (Test-Path "$ExportPath\ConditionalAccessPolicies")) {
+    $null = New-Item -Path "$ExportPath\ConditionalAccessPolicies" -ItemType Directory
+}
+
 $AllPolicies = Get-AzureADMSConditionalAccessPolicy
 
+Write-Output "Exporting Conditional Access Policies..." -ForegroundColor cyan
 foreach ($Policy in $AllPolicies) {
-    Write-Output "Backing up $($Policy.DisplayName)"
     $PolicyJSON = $Policy | ConvertTo-Json -Depth 6
-    $PolicyJSON | Out-File $ExportPath$($Policy.DisplayName).json
+    $PolicyJSON | Out-File $ExportPath\ConditionalAccessPolicies$($Policy.DisplayName).json
+    [PSCustomObject]@{
+        "Action" = "Export"
+        "Type"   = "Conditional Access Policy"
+        "Name"   = $Policy.DisplayName
+        "Path"   = "ConditionalAccessPolicies\$Policy.DisplayName.json"
+    }
 }
