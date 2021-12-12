6207811c5a39c2cfad5c9418a5a8ef3bb7659d18
Chris Palas <chris.palavouzis@gmail.com>
2021-12-12 15:45:06 +0200

    Break Glass Account modifications

--- Modules/InTunePal/Functions/Add-BreakGlassAccount.ps1
+++ Modules/InTunePal/Functions/Add-BreakGlassAccount.ps1
@@ -6,13 +6,30 @@ Function Add-BreakGlassAccount(){
 
     $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
     $PasswordProfile.Password = "P@55w.rd"
-
+    $UserPrincipalName = 'breakuser@' + $tenantforbreak
+    Write-Host "Creating the Break-Glass account with username: $UserPrincipalName and password: "$PasswordProfile.Password" !" -ForegroundColor Cyan
+    Write-Host
+    
     $Parameters= @{
         AccountEnabled = $true
         DisplayName = "Break-Glass Admin"
         PasswordProfile = $PasswordProfile
-        UserPrincipalName = breakuser@($tenantforbreak)
+        UserPrincipalName = $UserPrincipalName
+        MailNickName = "breakuser"
     }
 
-    New-AzureADUser @Parameters
+    $null = New-AzureADUser @Parameters
+
+    Write-Host "Break the glass account has been successfully created!" -ForegroundColor Cyan
+    Write-Host
+
+    $userassignment = Get-AzureADUser | Where-Object userPrincipalName -eq "breakuser@$tenantforbreak"
+
+    $roleDefinition = Get-AzureADMSRoleDefinition | Where-Object displayName -eq "Global Administrator"
+
+    $null = New-AzureADMSRoleAssignment -DirectoryScopeId '/' -RoleDefinitionId $roleDefinition.Id -PrincipalId $userassignment.objectId
+
+    Write-Host "Global Administrator role has been successfully assigned to the Break the glass account!" -ForegroundColor Cyan
+    Write-Host
+
 }
