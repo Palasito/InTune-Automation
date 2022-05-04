d2990d342009a3e18a153f9153ca5b21c2ed596a
Palasito <chris.palavouzis@gmail.com>
2022-05-04 13:19:15 +0300

    Beautification of Import

--- Modules/InTunePal/Functions/Import-EndpointSecurityPolicies.ps1
+++ Modules/InTunePal/Functions/Import-EndpointSecurityPolicies.ps1
@@ -1,7 +1,7 @@
-﻿Function Get-EndpointSecurityTemplate(){
+﻿Function Get-EndpointSecurityTemplate() {
 
-$graphApiVersion = "Beta"
-$ESP_resource = "deviceManagement/templates?`$filter=(isof(%27microsoft.graph.securityBaselineTemplate%27))"
+    $graphApiVersion = "Beta"
+    $ESP_resource = "deviceManagement/templates?`$filter=(isof(%27microsoft.graph.securityBaselineTemplate%27))"
 
     try {
 
@@ -12,16 +12,16 @@ $ESP_resource = "deviceManagement/templates?`$filter=(isof(%27microsoft.graph.se
     
     catch {
 
-    $ex = $_.Exception
-    $errorResponse = $ex.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorResponse)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-    Write-Host "Response content:`n$responseBody" -f Red
-    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-    write-host
-    break
+        $ex = $_.Exception
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+        Write-Host "Response content:`n$responseBody" -f Red
+        Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+        write-host
+        break
 
     }
 
@@ -29,34 +29,34 @@ $ESP_resource = "deviceManagement/templates?`$filter=(isof(%27microsoft.graph.se
 
 ####################################################
 
-Function Add-EndpointSecurityPolicy(){
+Function Add-EndpointSecurityPolicy() {
 
-[cmdletbinding()]
+    [cmdletbinding()]
 
-param
-(
-    $TemplateId,
-    $JSON
-)
+    param
+    (
+        $TemplateId,
+        $JSON
+    )
 
-$graphApiVersion = "Beta"
-$ESP_resource = "deviceManagement/templates/$TemplateId/createInstance"
-Write-Verbose "Resource: $ESP_resource"
+    $graphApiVersion = "Beta"
+    $ESP_resource = "deviceManagement/templates/$TemplateId/createInstance"
+    Write-Verbose "Resource: $ESP_resource"
 
     try {
 
-        if($JSON -eq "" -or $null -eq $JSON){
+        if ($JSON -eq "" -or $null -eq $JSON) {
 
-        write-host "No JSON specified, please specify valid JSON for the Endpoint Security Policy..." -f Red
+            write-host "No JSON specified, please specify valid JSON for the Endpoint Security Policy..." -f Red
 
         }
 
         else {
 
-        Test-JSON -JSON $JSON
+            Test-JSON -JSON $JSON
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($ESP_resource)"
-        Invoke-RestMethod -Uri $uri -Headers $authToken -Method Post -Body $JSON -ContentType "application/json"
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($ESP_resource)"
+            Invoke-RestMethod -Uri $uri -Headers $authToken -Method Post -Body $JSON -ContentType "application/json"
 
         }
 
@@ -64,16 +64,16 @@ Write-Verbose "Resource: $ESP_resource"
     
     catch {
 
-    $ex = $_.Exception
-    $errorResponse = $ex.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorResponse)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-    Write-Host "Response content:`n$responseBody" -f Red
-    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-    write-host
-    break
+        $ex = $_.Exception
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+        Write-Host "Response content:`n$responseBody" -f Red
+        Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+        write-host
+        break
 
     }
 
@@ -92,14 +92,14 @@ $Path = "C:\script\testout"
 $ImportPath = $Path
 
 # Replacing quotes for Test-Path
-$ImportPath = $ImportPath.replace('"','')
+$ImportPath = $ImportPath.replace('"', '')
 
-if(!(Test-Path "$ImportPath")){
+if (!(Test-Path "$ImportPath")) {
 
-Write-Host "Import Path for JSON file doesn't exist..." -ForegroundColor Red
-Write-Host "Script can't continue..." -ForegroundColor Red
-Write-Host
-break
+    Write-Host "Import Path for JSON file doesn't exist..." -ForegroundColor Red
+    Write-Host "Script can't continue..." -ForegroundColor Red
+    Write-Host
+    break
 
 }
 
@@ -123,12 +123,12 @@ $Templates = Get-EndpointSecurityTemplate
 ####################################################
 
 # Checking if templateId from JSON is a valid templateId
-$ES_Template = $Templates | Where-Object  { $_.id -eq $JSON_TemplateId }
+$ES_Template = $Templates | Where-Object { $_.id -eq $JSON_TemplateId }
 
 ####################################################
 
 # If template is a baseline Edge, MDATP or Windows, use templateId specified
-if(($ES_Template.templateType -eq "microsoftEdgeSecurityBaseline") -or ($ES_Template.templateType -eq "securityBaseline") -or ($ES_Template.templateType -eq "advancedThreatProtectionSecurityBaseline")){
+if (($ES_Template.templateType -eq "microsoftEdgeSecurityBaseline") -or ($ES_Template.templateType -eq "securityBaseline") -or ($ES_Template.templateType -eq "advancedThreatProtectionSecurityBaseline")) {
 
     $TemplateId = $JSON_Convert.templateId
 
@@ -137,17 +137,17 @@ if(($ES_Template.templateType -eq "microsoftEdgeSecurityBaseline") -or ($ES_Temp
 ####################################################
 
 # Else If not a baseline, check if template is deprecated
-elseif($ES_Template){
+elseif ($ES_Template) {
 
     # if template isn't deprecated use templateId
-    if($ES_Template.isDeprecated -eq $false){
+    if ($ES_Template.isDeprecated -eq $false) {
 
         $TemplateId = $JSON_Convert.templateId
 
     }
 
     # If template deprecated, look for lastest version
-    elseif($ES_Template.isDeprecated -eq $true) {
+    elseif ($ES_Template.isDeprecated -eq $true) {
 
         $Template = $Templates | Where-Object { $_.displayName -eq "$JSON_TemplateDisplayName" }
 
@@ -162,13 +162,13 @@ elseif($ES_Template){
 ####################################################
 
 # Else If Imported JSON template ID can't be found check if Template Display Name can be used
-elseif($null -eq $ES_Template){
+elseif ($null -eq $ES_Template) {
 
-    $ES_Template = $Templates | Where-Object  { $_.displayName -eq "$JSON_TemplateDisplayName" }
+    $ES_Template = $Templates | Where-Object { $_.displayName -eq "$JSON_TemplateDisplayName" }
 
-    If($ES_Template){
+    If ($ES_Template) {
 
-        if(($ES_Template.templateType -eq "securityBaseline") -or ($ES_Template.templateType -eq "advancedThreatProtectionSecurityBaseline")){
+        if (($ES_Template.templateType -eq "securityBaseline") -or ($ES_Template.templateType -eq "advancedThreatProtectionSecurityBaseline")) {
 
             Write-Host
             Write-Host "TemplateID '$JSON_TemplateId' with template Name '$JSON_TemplateDisplayName' doesn't exist..." -ForegroundColor Red
@@ -203,7 +203,7 @@ elseif($null -eq $ES_Template){
 ####################################################
 
 # Excluding certain properties from JSON that aren't required for import
-$JSON_Convert = $JSON_Convert | Select-Object -Property * -ExcludeProperty TemplateDisplayName,TemplateId,versionInfo
+$JSON_Convert = $JSON_Convert | Select-Object -Property * -ExcludeProperty TemplateDisplayName, TemplateId, versionInfo
 
 $DisplayName = $JSON_Convert.DisplayName
 
