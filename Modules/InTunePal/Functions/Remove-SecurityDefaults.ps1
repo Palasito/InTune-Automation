8c766441f39030c2f53fe69da8c48cc97790a0d9
Palasito <chris.palavouzis@gmail.com>
2022-12-22 15:41:39 +0200

    SecDefaults

---
+++ Modules/InTunePal/Functions/Remove-SecurityDefaults.ps1
@@ -0,0 +1,22 @@
+Function Remove-SecurityDefaults() {
+
+    try {
+        $uri = "https://graph.microsoft.com/beta/policies/identitySecurityDefaultsEnforcementPolicy"
+        $Settings = Invoke-WebRequest -Headers $authToken -Uri "https://graph.microsoft.com/beta/policies/identitySecurityDefaultsEnforcementPolicy"
+        $Settings = $Settings | ConvertFrom-Json
+        if ($Settings.isEnabled -eq "true") {
+            $body = (@{"isEnabled" = "false" } | ConvertTo-Json)
+            $null = Invoke-RestMethod -Method Patch -Headers $authToken -Uri $uri -Body $body
+            Write-Host "Security Defaults have been disabled !" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Security defaults are already disabled, will not make any changes..." -ForegroundColor Cyan
+        }
+    }
+    catch {
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
+        break
+    }
+}
