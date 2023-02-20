32b340297deba06bdebb191f808339058c341d33
Palasito <chris.palavouzis@gmail.com>
2023-02-20 16:12:53 +0200

    MSAL.PS required + enterpriseapp

---
+++ Modules/InTunePal/Functions/RemoveEnterpriseApp.ps1
@@ -0,0 +1,18 @@
+function RemoveEnterpriseApp {
+
+    [cmdletbinding()]
+    param (
+
+    )
+
+    try {
+        $null = Invoke-RestMethod -Method Delete -Headers $authToken -Uri "https://graph.microsoft.com/v1.0/servicePrincipals(appId='d1ddf0e4-d672-4dae-b554-9d5bdfd93547')"
+        Write-Host "Enterprise Application has been Successfully deleted" -ForegroundColor Green
+    }
+
+    catch {
+        Write-Host "Enterprise Application could not be removed!"
+        Write-Host "$_`n"
+        break
+    }
+}
