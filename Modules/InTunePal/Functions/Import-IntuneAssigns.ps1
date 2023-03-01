fa2a7b10e3c9a02e66033a177778963a1a085141
Palasito <chris.palavouzis@gmail.com>
2023-03-01 14:58:01 +0200

    Renamed Fucnction

---
+++ Modules/InTunePal/Functions/Import-IntuneAssigns.ps1
@@ -0,0 +1,52 @@
+function Import-IntuneAssigns {
+
+    [cmdletbinding()]
+
+    param(
+        [Parameter(mandatory)]
+        $Path,
+        [switch]$Token,
+        [switch]$AADGroups,
+        [switch]$CAPGroups,
+        [switch]$CPGroups,
+        [switch]$DCPGroups,
+        [switch]$DUPGroups,
+        [switch]$ApplicationGroups,
+        [switch]$APPGroups,
+        [switch]$EndpointSecGroups
+    )
+
+    Write-Host
+    Write-Host "Getting Ready to assign AzureAD Groups to the imported configuration..." -ForegroundColor Cyan
+
+    #Region Authentication
+    $null = Get-Token
+    #EndRegion
+
+    #Region Assignments
+    Write-Host "Creating Assignments as specified in "$Path\CSVs" folder..." -ForegroundColor Cyan
+
+    if ($AADGroups) { Import-AADGroups -Path $Path }
+
+    if ($CAPGroups) { Add-CAPGroups -Path $Path }
+
+    if ($CPGroups) { Add-CPGroups -Path $Path }
+    
+    if ($DCPGroups) { Add-DCPGroups -Path $Path }
+
+    if ($DUPGroups) { Add-DUPGroups -Path $Path }
+
+    if ($APPGroups) { Add-APPGroups -Path $Path }
+    #EndRegion
+
+    #Region Continue or Exit
+    $confirmation = Read-Host "Do you want to perform another job? [y/n]"
+    if ($confirmation -eq 'n') {
+        Write-Host "Thanks for using InTunePal! Have a nice one!" -ForegroundColor Green
+        break;
+    }
+    if ($confirmation -eq 'y') {
+        Start-InTuneModule
+    }
+    #EndRegion
+}
