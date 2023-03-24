f152e29482f3b5a37ba02dbd4435a06b2ccdbfc3
Palasito <chris.palavouzis@gmail.com>
2023-03-24 12:55:33 +0200

    Added function for folder

---
+++ Modules/InTunePal/Functions/Get-FolderName.ps1
@@ -0,0 +1,31 @@
+function Get-FolderName {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [string]$Message = "Select working directory!",
+        [string]$InitialDirectory = [System.Environment+SpecialFolder]::MyComputer,
+        [switch]$ShowNewFolderButton
+    )
+
+    $browserForFolderOptions = 0x00000041                                  # BIF_RETURNONLYFSDIRS -bor BIF_NEWDIALOGSTYLE
+    if (!$ShowNewFolderButton) { $browserForFolderOptions += 0x00000200 }  # BIF_NONEWFOLDERBUTTON
+
+    $browser = New-Object -ComObject Shell.Application
+    # To make the dialog topmost, you need to supply the Window handle of the current process
+    [intPtr]$handle = [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
+
+    # see: https://msdn.microsoft.com/en-us/library/windows/desktop/bb773205(v=vs.85).aspx
+    $folder = $browser.BrowseForFolder($handle, $Message, $browserForFolderOptions, $InitialDirectory)
+
+    $result = $null
+    if ($folder) {
+        $result = $folder.Self.Path
+    }
+
+    # Release and remove the used Com object from memory
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($browser) | Out-Null
+    [System.GC]::Collect()
+    [System.GC]::WaitForPendingFinalizers()
+
+    return $result
+}
